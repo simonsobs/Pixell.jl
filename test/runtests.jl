41a4c0f4ead4493fa96a520161fea7070e3474ff
xzackli <xzackli@gmail.com>
2022-01-22 21:12:12 -0500

    add pixell install

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using Pixell
 using Test
 
+using PyCall
+enmap = pyimport("pixell.enmap")
+
 @testset "Pixell.jl" begin
     # Write your tests here.
+    shape, wcs_py = enmap.geometry(shape=(128, 256), res=deg2rad(5/60.),pos=(0,0))
+    m = pycall(enmap.ones, PyObject, shape, wcs_py)
 end
