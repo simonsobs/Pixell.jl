6502d57ad16c6a1a0e1e454f615133b5d473ce2e
xzackli <xzackli@gmail.com>
2022-02-07 03:04:59 -0500

    basic geometry handling with units

--- src/Pixell.jl
+++ src/Pixell.jl
@@ -4,13 +4,21 @@ using WCS
 using FITSIO
 using FFTW
 using Printf
+import Unitful, UnitfulAngles
+import Unitful: uconvert, ustrip
 
 include("enmap.jl")
+include("enmap_geom.jl")
 include("enmap_ops.jl")
 
 export Enmap, CarClenshawCurtis, getwcs
-export fullsky_geometry, slice_geometry
+export geometry, fullsky_geometry, slice_geometry
 export pix2sky, pix2sky!, sky2pix, sky2pix!
 export read_map, write_map
+
+# set up some shortcuts for common angles
+const radian = Unitful.rad
+const degree = Unitful.°
+const arcminute = UnitfulAngles.arcminute
 
 end
