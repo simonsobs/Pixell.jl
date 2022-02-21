411832c956ef230881009d3b77004fe1e4ca1291
xzackli <xzackli@gmail.com>
2022-02-21 01:55:05 -0500

    add spin 2 SHTs

--- src/transforms.jl
+++ src/transforms.jl
@@ -66,9 +66,12 @@ function create_sht_band(m::Enmap)
     shape = size(m)
     flipx_slice, flipy_slice = get_flip_slices(shape, getwcs(m))
     _, wcs = slice_geometry(shape, getwcs(m), flipx_slice, flipy_slice, shape[3:end])
-    target_size = (fullringsize(wcs), size(m,2))
+    target_size = (fullringsize(wcs), size(m,2), shape[3:end]...)
     converted = Enmap(zeros(Float64, target_size), wcs)
-    converted.data[1:shape[1],:] .= view(m.data, flipx_slice, flipy_slice)
+    other_slices = axes(m)[3:end]
+
+    # extend the ring by adding more columns to the end
+    converted.data[1:shape[1], :, other_slices...] .= view(m.data, flipx_slice, flipy_slice, other_slices...)
     return converted
 end
 
@@ -98,3 +101,90 @@ function map2alm(input_map::Enmap{T,2}; lmax=nothing, mmax=lmax) where T
     return alm
 end
 
+# spin 2 transforms ----
+
+# 2-tuple of scalar enmaps means QU transform
+function map2alm(maps::NTuple{2,EM}; lmax=nothing, mmax=lmax) where {T, EM<:Enmap{T,2}}
+    if isnothing(lmax)
+        lmax = getlmax(getwcs(first(maps)))
+        mmax = lmax
+    end
+    band_maps = create_sht_band.(maps)
+    geom_info = make_cc_geom_info(size(band_maps[1]), getwcs(band_maps[1]))
+    npix = map_size(geom_info)
+    flat_maps = [reshape(m.data, npix) for m in band_maps]
+
+    alm_info = make_triangular_alm_info(lmax, mmax, 1)
+    nalm = alm_count(alm_info)
+    alms = [Alm(lmax, mmax, zeros(ComplexF64, nalm)) for m in band_maps]
+
+    sharp_execute!(
+        SHARP_MAP2ALM, 2, 
+        [a.alm for a in alms], 
+        flat_maps,
+        geom_info, alm_info, SHARP_DP)
+
+    return alms[1], alms[2]
+end
+
+# 3-tuple of scalar enmaps means IQU transform
+function map2alm(maps::NTuple{3,EM}; lmax=nothing, mmax=lmax) where {T, EM<:Enmap{T,2}}
+    if isnothing(lmax)
+        lmax = getlmax(getwcs(first(maps)))
+        mmax = lmax
+    end
+    alm_i = map2alm(maps[1]; lmax=lmax, mmax=mmax)
+    alm_e, alm_b = map2alm(maps[2:3]; lmax=lmax, mmax=mmax)
+    return alm_i, alm_e, alm_b
+end
+
+# handle enmaps that are stacks of I, QU, and IQU in the third dim
+function map2alm(input_map::Enmap{T,3}; lmax=nothing, mmax=lmax) where T
+    if isnothing(lmax)
+        lmax = getlmax(getwcs(input_map))
+        mmax = lmax
+    end
+    if size(input_map,3) == 1  # this I map for some reason has a single third dim
+        sliced_1d_map = @view input_map[:,:,1]
+        return map2alm(sliced_1d_map; lmax=lmax, mmax=mmax)
+    elseif size(input_map,3) == 2  # QU map
+        return _map2almQU(input_map; lmax=lmax, mmax=mmax)
+    elseif size(input_map,3) == 3  # IQU map
+        alm_t = map2alm(@view(input_map[:,:,1]); lmax=lmax, mmax=mmax)
+        alm_e, alm_b = _map2almQU(@view(input_map[:,:,2:3]); lmax=lmax, mmax=mmax)
+        return alm_t, alm_e, alm_b
+    end
+    throw(ArgumentError("SHTs require shape (nx,ny,ncomp) with 1 ≤ ncomp ≤ 3, for I, QU, and IQU."))
+end
+
+# special case QU: have to manually ccall with the columns when 
+# Enmap is a stack (nx, ny, 2)
+# (internal) perform forward SHT of an QU map
+function _map2almQU(input_map::Enmap{T,3}; lmax, mmax) where T
+
+    @assert size(input_map,3) == 2  # check that we have Q and U channels
+
+    band_map = create_sht_band(input_map)
+    geom_info = make_cc_geom_info(size(band_map), getwcs(band_map))
+    npix = map_size(geom_info)
+    flat_map = reshape(band_map.data, npix * 2)
+
+    alm_info = make_triangular_alm_info(lmax, mmax, 1)
+    nalm = alm_count(alm_info)
+    alm_e = Alm(lmax, mmax, zeros(ComplexF64, nalm))
+    alm_b = Alm(lmax, mmax, zeros(ComplexF64, nalm))
+
+    # have to manually ccall here since I didn't add the pointers directly
+    GC.@preserve alm_e alm_b flat_map ccall(
+        (:sharp_execute, Libsharp.libsharp2),
+        Cvoid,
+        (Cint, Cint, Ptr{Cvoid}, Ptr{Cvoid}, Ptr{Cvoid}, Ptr{Cvoid}, Cint,
+            Ref{Cdouble}, Ref{Culonglong}),
+        SHARP_MAP2ALM, 2, 
+        [pointer(alm_e.alm), pointer(alm_b.alm)], 
+        [pointer(flat_map, 1), pointer(flat_map, 1+npix)],
+        geom_info.ptr, alm_info.ptr, SHARP_DP,
+        Ptr{Cdouble}(C_NULL), Ptr{Culonglong}(C_NULL))
+
+    return alm_e, alm_b
+end
