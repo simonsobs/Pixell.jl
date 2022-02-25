615dad8745a3e043eba219fb87ff11b3299df870
xzackli <xzackli@gmail.com>
2022-02-25 02:57:10 -0800

    fix more io tests

--- test/test_io.jl
+++ test/test_io.jl
@@ -1,14 +1,16 @@
 
 
 @testset "Enmap I/O" begin
-    imap = read_map("data/test.fits")
+    imap = read_map("data/test.fits"; trim=false)
     @test size(imap) == (100, 100, 3)
     @test imap.wcs.naxis == 2
     @test imap.wcs.cdelt == [-1, 1]
     @test imap.wcs.crval == [0.5, 0.0]
     @test sum(imap) ≈ 14967.2985
     # read with sel
-    imap = read_map("data/test.fits", sel=(11:20,21:40,1:2))
+    imap = read_map("data/test.fits", sel=(11:20,21:40,1:2); trim=false)
+    @test size(imap) == (10,20,2)
+    imap = read_map("data/test.fits", sel=(11:20,21:40,1:2); trim=true)
     @test size(imap) == (10,20,2)
     # todo: add tests on IAU conversion w/ and w/o sel
 end
