ca87343589ef0b095b3089a008c33abab034eea8
Louis Branch <louis.branch@mail.utoronto.ca>
2022-08-19 08:15:27 -0400

    Use approximate values for tests

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,4 +1,4 @@
 @testset "dplanck" begin
-    @test dplanck(98e9) == 231581854 atol = 100
-    @test dplanck(150e9) == 398477703 atol = 100
+    @test dplanck(98e9) ≈ 231581854 atol = 100
+    @test dplanck(150e9) ≈ 398477703 atol = 100
 end
